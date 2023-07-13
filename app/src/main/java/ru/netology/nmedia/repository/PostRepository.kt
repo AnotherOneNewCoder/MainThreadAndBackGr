426faf894addb02de9d83f4d3ee3f04825903022
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2023-07-13 19:43:45 +0300

    flow done

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -2,15 +2,19 @@ package ru.netology.nmedia.repository
 
 
 import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    val data: LiveData<List<Post>>
+    val data: Flow<List<Post>>
     suspend fun getAll()
+
+    fun getNewerCount(id: Long) : Flow<Int>
     suspend fun save(post: Post)
 
     suspend fun likeById(id: Long)
     suspend fun removeById(id: Long)
 
     suspend fun unlikeByID(id: Long)
+    suspend fun getAllUnhide()
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -2,22 +2,32 @@ package ru.netology.nmedia.repository
 
 
 
-import androidx.lifecycle.map
 
-import okio.IOException
-import ru.netology.nmedia.api.PostApi
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+import ru.netology.nmedia.api.*
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.entity.PostEntity
 import ru.netology.nmedia.entity.toDto
 import ru.netology.nmedia.entity.toEntity
-import ru.netology.nmedia.error.*
+import ru.netology.nmedia.error.ApiError
+import ru.netology.nmedia.error.AppError
+import ru.netology.nmedia.error.NetworkError
+import ru.netology.nmedia.error.UnknownError
+import java.io.IOException
 
 
 class PostRepositoryImpl(
     private val dao: PostDao
 ) : PostRepository {
-    override val data = dao.getAll().map(List<PostEntity>::toDto)
+
+    override val data = dao.getAllVisible()
+        .map(List<PostEntity>::toDto)
+        .flowOn(Dispatchers.Default)
 
     override suspend fun getAll() {
         try {
@@ -27,7 +37,7 @@ class PostRepositoryImpl(
             }
             val body = response.body() ?: throw ApiError(response.code(), response.message())
             val savedPosts = body.map { it.copy(saved = true) }
-            dao.insert(savedPosts.toEntity())
+            dao.insert(savedPosts.toEntity(hidden = false))
 
         } catch (e: IOException) {
             throw NetworkError
@@ -36,6 +46,23 @@ class PostRepositoryImpl(
         }
 
     }
+
+    override fun getNewerCount(id: Long): Flow<Int> = flow {
+        while (true) {
+            delay(10_000L)
+            val response = PostApi.retrofitService.getNewer(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            dao.insert(body.toEntity(hidden = true))
+            //emit(body.size)
+            emit(dao.count())
+        }
+    }
+        .catch { e -> throw AppError.from(e) }
+        .flowOn(Dispatchers.Default)
 
     override suspend fun save(post: Post) {
         val newPostId = post.copy(id = 9_223_372_036_854_775_800)
@@ -116,6 +143,12 @@ class PostRepositoryImpl(
         } catch (e: Exception) {
             throw UnknownError
         }
+    }
+    override suspend fun getAllUnhide() {
+        CoroutineScope(Dispatchers.Default).launch {
+            dao.getAllUnhide()
+        }
+
     }
 
 
